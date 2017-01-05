09745cb36fc8603de48608d9da63426a213d42a2
jay raparla <dhananjay.raparla@gmail.com>
2017-01-05 10:11:22 -0500

    liquibase change logs missing in git.. pushing them

---
+++ src/main/java/com/jay/app/domain/WishList.java
@@ -0,0 +1,128 @@
+package com.jay.app.domain;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+/**
+ * A WishList.
+ */
+@Entity
+@Table(name = "wish_list")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+public class WishList implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "creation_date")
+    private LocalDate creationDate;
+
+    @Column(name = "is_public")
+    private Boolean isPublic;
+
+    @ManyToOne
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public WishList name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public WishList creationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+        return this;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Boolean isIsPublic() {
+        return isPublic;
+    }
+
+    public WishList isPublic(Boolean isPublic) {
+        this.isPublic = isPublic;
+        return this;
+    }
+
+    public void setIsPublic(Boolean isPublic) {
+        this.isPublic = isPublic;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public WishList user(User user) {
+        this.user = user;
+        return this;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        WishList wishList = (WishList) o;
+        if (wishList.id == null || id == null) {
+            return false;
+        }
+        return Objects.equals(id, wishList.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "WishList{" +
+            "id=" + id +
+            ", name='" + name + "'" +
+            ", creationDate='" + creationDate + "'" +
+            ", isPublic='" + isPublic + "'" +
+            '}';
+    }
+}
